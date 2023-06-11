4c8ef22ae8814558a1e425dd2d7ac66a37c5fb99
kharoldcanova <kharoldsamir@gmail.com>
2023-06-11 16:52:27 -0500

    improvements in theme app

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  //texttheme
+  TextTheme textTheme = GoogleFonts.poppinsTextTheme(
+    const TextTheme(
+
+    )
+  );
   //theme function
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
     brightness: Brightness.dark,
-    colorSchemeSeed: Colors.black
+    colorSchemeSeed: Colors.blueAccent[700],
+    textTheme: textTheme,
   );
 }
+
+
+  
+
