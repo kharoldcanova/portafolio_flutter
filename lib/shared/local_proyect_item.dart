3e65ea666872941327ba4c6a62407b4ac4e1d16e
kharoldcanova <kharoldsamir@gmail.com>
2023-06-13 13:30:00 -0500

    information upload

--- lib/shared/local_proyect_item.dart
+++ lib/shared/local_proyect_item.dart
@@ -1,7 +1,74 @@
 List<Map<String, dynamic>> proyectItem = [
   {
     'name': 'Contador basico',
-    'description': 'Un proyecto que expone el uso de Statefull Widget',
-    'linkUrl': '23230',
+    'description': 'Un proyecto que expone el uso de Statefull Widget para gestionar el estado de la aplicacion.',
+    'linkUrl': 'https://github.com/kharoldcanova/devtalls_mobile',
+    'language': 'Flutter',
+  },
+  {
+    'name': 'Login con Flutter',
+    'description': 'Realizacion de una interfaz atractiva de un diseño creado en Figma con funcionalidad de autenticacion usando los servicios de Firebase.',
+    'linkUrl': 'https://github.com/kharoldcanova/login_flutter',
+    'language': 'Flutter',
+  },
+  {
+    'name': 'Flutter Platzi',
+    'description': 'Realizacion de Widgets personalizados para complementar una interfaz de calificacion de lugares. ',
+    'linkUrl': 'https://github.com/kharoldcanova/flutter_platzi/tree/master',
+    'language': 'Flutter',
+  },
+  {
+    'name': 'Portafolio Flutter',
+    'description': 'Una interfaz de un portafolio de ejemplo responsivo usando la libreria Velocity X',
+    'linkUrl': 'https://github.com/kharoldcanova/resume_flutter',
+    'language': 'Flutter',
+  },
+  {
+    'name': 'ChatBot Flutter',
+    'description': 'Un chatbot que consume una API de respuestas basadas en una imagen de Yes o No usando la libreria de Dio',
+    'linkUrl': 'https://github.com/kharoldcanova/resume_flutter',
+    'language': 'Flutter',
+  },
+  {
+    'name': 'Busqueda con Wikipedia',
+    'description': 'Codigo de terminal que usa la librerias de Wikipedia y gTTS para realizar uns busqueda de texto a voz.',
+    'linkUrl': 'https://github.com/kharoldcanova/busquedaWikipedia',
+    'language': 'Python',
+  },
+  {
+    'name': 'Analizar Web',
+    'description': 'Codigo de terminal que analiza las tecnologias usadas en una pagina web.',
+    'linkUrl': 'https://github.com/kharoldcanova/analizarWeb',
+    'language': 'Python',
+  },
+  {
+    'name': 'Asistente de Voz',
+    'description': 'Asistente con reconocimiento de voz que permite realizar tareas sencillas.',
+    'linkUrl': 'https://github.com/kharoldcanova/asistenteVoz',
+    'language': 'Python',
+  },
+  {
+    'name': 'React ToDo',
+    'description': 'Pagina web sencilla para registrar y listar tareas realizada con React usando el lenguajde JavaScript',
+    'linkUrl': 'https://github.com/kharoldcanova/react-todo',
+    'language': 'JavaScript',
+  },
+  {
+    'name': 'Portafolio Kharold Canova',
+    'description': 'Un portafolio profesional creado en React usando la libreria de Boostrap.',
+    'linkUrl': 'https://github.com/kharoldcanova/kharold_canova',
+    'language': 'JavaScript',
+  },
+  {
+    'name': 'Traductor Basico',
+    'description': 'Un codigo de consola que permite traducir frases de Español a Ingles usando la libreria de Deep Translate.',
+    'linkUrl': 'https://github.com/kharoldcanova/traductorBasico',
+    'language': 'Python',
+  },
+  {
+    'name': 'Edit Image',
+    'description': 'Un codigo de consola para probar algunos filtros que proporciona la libreria Pillow.',
+    'linkUrl': 'https://github.com/kharoldcanova/editImage',
+    'language': 'Python',
   },
 ];
