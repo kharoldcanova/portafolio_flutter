878af0a25ea346770cfbc3c713f0b93eab28d24f
kharoldcanova <kharoldsamir@gmail.com>
2023-06-15 14:16:49 -0500

    optimiced code

---
+++ lib/shared/local_about_me.dart
@@ -0,0 +1,14 @@
+const aboutMeLocal = [
+  {
+    "presentation":
+        "¡Hola! Soy Kharold Canova, un apasionado desarrollador con alrededor de dos años de experiencia en el uso de Flutter, el framework de desarrollo de aplicaciones móviles multiplataforma. Mi objetivo es aportar mis habilidades y conocimientos a una organización que valore la innovación y la excelencia en el desarrollo de software.",
+    "interesting":
+        "Además de mi experiencia en Flutter, también tengo un profundo interés en el campo de la inteligencia artificial. Me fascina la capacidad de las máquinas para aprender y tomar decisiones inteligentes, y me encantaría trabajar en proyectos relacionados con chatbots como Bard o sistemas de procesamiento de lenguaje natural como ChatGPT. Creo firmemente que la inteligencia artificial tiene el potencial de transformar nuestra forma de interactuar con la tecnología y mejorar la vida de las personas.",
+    "love":
+        "Fuera del mundo de la tecnología, tengo una pasión desbordante por las películas de ciencia ficción. Disfruto sumergirme en mundos imaginarios y explorar las posibilidades futuristas que nos muestran. Esta pasión por la ciencia ficción alimenta mi creatividad y mi pensamiento fuera de lo convencional, lo que me permite abordar los desafíos de desarrollo de software desde diferentes perspectivas.",
+    "habilities":
+        "Además de programar, tengo habilidades en la escritura creativa. Me encanta componer poemas y escribir relatos cortos. Esta habilidad me permite expresar mi creatividad y ver el mundo desde una perspectiva artística, lo cual creo que es esencial en el desarrollo de software. Creo firmemente que la combinación de habilidades técnicas y creativas me hace un candidato único para aportar ideas frescas y soluciones innovadoras a cualquier proyecto.",
+    "message":
+        "Estoy emocionado por la posibilidad de formar parte de un equipo dinámico y trabajar en proyectos que marquen la diferencia. Estoy comprometido con el aprendizaje continuo y el crecimiento profesional, y estoy seguro de que mi experiencia en Flutter, mi pasión por la inteligencia artificial y mis habilidades creativas pueden hacer una contribución significativa a su organización. ¡Espero con ansias la oportunidad de hablar más sobre cómo puedo agregar valor a su equipo!"
+  }
+];
